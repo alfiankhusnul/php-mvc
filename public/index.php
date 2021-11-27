8b008e293db3b8c5096dff994d333bdf20e9da22
Alfian Khusnul Amar <alfiankhusnul@gmail.com>
2021-11-27 20:29:32 +0800

    Menambahkan controller ke mvc

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use AlfianKhusnul\PHP\MVC\App\Router;
+use AlfianKhusnul\PHP\MVC\Controller\HomeController;
 
-Router::add('GET', '/', 'HomeController', 'index');
-Router::add('GET', '/login', 'UserController', 'login');
-Router::add('GET', '/register', 'UserController', 'register');
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+Router::add('GET', '/world', HomeController::class, 'world');
 
 Router::run();
