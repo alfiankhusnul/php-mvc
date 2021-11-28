dba8dfdf1e41868a0cab8285943237d2d37d4f93
Alfian Khusnul Amar <alfiankhusnul@gmail.com>
2021-11-28 10:44:36 +0800

    Menambahkan path variable menggunakan regex

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use AlfianKhusnul\PHP\MVC\App\Router;
 use AlfianKhusnul\PHP\MVC\Controller\HomeController;
+use AlfianKhusnul\PHP\MVC\Controller\ProductController;
+
+Router::add('GET', '/product/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
 
 Router::add('GET', '/', HomeController::class, 'index');
 Router::add('GET', '/hello', HomeController::class, 'hello');
